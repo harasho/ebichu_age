c530d446dad9e19efc259d3395a47f698f8979a6
masakazu takasho <takashoster@gmail.com>
2018-11-28 11:48:09 +0900

    インタラクティブにした

--- ebi.rb
+++ ebi.rb
@@ -16,7 +16,13 @@ def ebi_age(year, month, day)
   puts "あなたは中学#{chugaku}年生です"
 end
 
-members = ["真山りか", "安本彩花", "廣田あいか", "星名美怜", "松野莉奈", "柏木ひなた", "小林歌穂", "中山莉子"]
-puts "私立恵比寿中学のメンバーには#{members.sample}がいます"
+#members = ["真山りか", "安本彩花", "廣田あいか", "星名美怜", "松野莉奈", "柏木ひなた", "小林歌穂", "中山莉子"]
+#puts "私立恵比寿中学のメンバーには#{members.sample}がいます"
 
-ebi_age(2002,3,11)
+puts("西暦何年生まれですか")
+year = gets.chomp.to_i
+puts("何月生まれですか")
+month = gets.chomp.to_i
+puts("何日生まれですか")
+day = gets.chomp.to_i
+ebi_age(year, month, day)
